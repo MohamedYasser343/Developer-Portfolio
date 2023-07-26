0fbd3fe1410529223101c365161c04b07210fcca
Mohamed Yasser <96927790+MohamedYasser343@users.noreply.github.com>
2023-07-26 14:26:59 +0300

    Add files via upload

---
+++ src/components/About.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        I am a highly skilled Full Stack Developer with expertise in Django, React, and React Native.
+        Passionate about creating seamless and innovative web and mobile applications,
+        I thrive in leveraging my technical prowess to deliver high-quality solutions that exceed client expectations.
+        With a solid foundation in both backend and frontend development,
+        I possess a comprehensive understanding of building robust and scalable systems.
+        My commitment to staying up-to-date with the latest technologies and industry trends allows me to continuously enhance my skills and deliver cutting-edge products.
+        As a motivated and adaptable developer, I am driven to tackle new challenges and contribute my expertise to projects that make a meaningful impact.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
